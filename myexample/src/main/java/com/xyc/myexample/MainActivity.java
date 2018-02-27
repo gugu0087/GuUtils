d036c23594c90775196e357f0f46adf4c497f797
许运昌 <xuyunchang@mzjmedia.net>
2018-02-27 15:32:42 +0800

    增加线程池管理

--- myexample/src/main/java/com/xyc/myexample/MainActivity.java
+++ myexample/src/main/java/com/xyc/myexample/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.xyc.okutils.manager.ThreadPoolManager;
 import com.xyc.okutils.model.NetLogModel;
 import com.xyc.okutils.utils.NetLogManager;
 
@@ -20,5 +21,12 @@ public class MainActivity extends AppCompatActivity {
         netLogModel.setMethod("post");
         netLogModel.setContent("sdadad");
         NetLogManager.getInstance().logNetResponse(netLogModel,true);
+
+        ThreadPoolManager.getInstance().getFixedThreadPool(5).execute(new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        });
     }
 }
